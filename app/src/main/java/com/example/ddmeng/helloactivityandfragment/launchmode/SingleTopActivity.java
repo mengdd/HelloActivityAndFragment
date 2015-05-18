e39cb26666fdaa3288f7b996bfa34741f71e579e
Dandan MENG <meng.dandan@rea-group.com>
2015-05-18 14:31:21 +0800

    finish singleTop activity test and get some conclusions

--- app/src/main/java/com/example/ddmeng/helloactivityandfragment/launchmode/SingleTopActivity.java
+++ app/src/main/java/com/example/ddmeng/helloactivityandfragment/launchmode/SingleTopActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 
 import com.example.ddmeng.helloactivityandfragment.R;
+import com.example.ddmeng.helloactivityandfragment.utils.TaskUtils;
 
 public class SingleTopActivity extends Activity {
 
@@ -16,19 +17,47 @@ public class SingleTopActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.launch_mode_single_top_activity);
 
-        Log.i(LOG_TAG, "Single Top Activity, onCreate()");
+        Log.i(LOG_TAG, "Single Top Activity, onCreate(), " + this.hashCode());
 
         //strange phenomenon:
         // When start this activity, startActivity() is called twice deliberately
         // then the onCreate() is called once (right).
         // But when press back, the onCreate() is called again (strange)
         // So using normal eyes, the singleTop activity appear two times.
+
+        //Conclusions: Do NOT call startActivity() twice
+        // Because before onCreate() invoked, the singleTop activity are still not considered as on the top of the stack
     }
 
     @Override
     protected void onNewIntent(Intent intent) {
         super.onNewIntent(intent);
-        Log.i(LOG_TAG, "Single Top Activity, onNewIntent()");
+        Log.i(LOG_TAG, "Single Top Activity, onNewIntent(), " + this.hashCode());
         //strange: this log is never shown
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(LOG_TAG, "Single Top Activity, onResume(), " + this.hashCode());
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(LOG_TAG, "Single Top Activity, onPause(), " + this.hashCode());
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(LOG_TAG, "Single Top Activity, onStop(), " + this.hashCode());
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(LOG_TAG, "Single Top Activity, onDestroy(), " + this.hashCode());
     }
 }
--- app/src/main/java/com/example/ddmeng/helloactivityandfragment/launchmode/StandardActivity.java
+++ app/src/main/java/com/example/ddmeng/helloactivityandfragment/launchmode/StandardActivity.java
@@ -6,15 +6,19 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.ddmeng.helloactivityandfragment.R;
+import com.example.ddmeng.helloactivityandfragment.utils.TaskUtils;
 
 public class StandardActivity extends Activity {
     private static final String LOG_TAG = "Launch Mode";
+    private TextView mStackInfo = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.i(LOG_TAG, "Standard Activity onCreate()");
         setContentView(R.layout.launch_mode_standard_activity);
         Button startSingleTopButton = (Button) findViewById(R.id.launch_mode_start_single_top_button);
         startSingleTopButton.setOnClickListener(new View.OnClickListener() {
@@ -25,9 +29,41 @@ public class StandardActivity extends Activity {
                 Intent intent = new Intent();
                 intent.setClass(StandardActivity.this, SingleTopActivity.class);
                 startActivity(intent);
-                //start twice
-                startActivity(intent);
+
             }
         });
+
+        mStackInfo = (TextView) findViewById(R.id.launch_mode_stack_info);
+        updateStackInfo();
+    }
+
+    private void updateStackInfo() {
+        String currentTopActivityName = TaskUtils.getCurrentTopActivityName(this);
+        CharSequence info = getResources().getString(R.string.launch_mode_stack_info) + ", top Activity: " + currentTopActivityName;
+        mStackInfo.setText(info);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(LOG_TAG, this.getClass().getSimpleName() + " onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(LOG_TAG, this.getClass().getSimpleName() + " onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(LOG_TAG, this.getClass().getSimpleName() + " onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(LOG_TAG, this.getClass().getSimpleName() + " onDestroy()");
     }
 }
